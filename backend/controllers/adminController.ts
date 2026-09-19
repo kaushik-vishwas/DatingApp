9edd81e1343eab325fb53a1762dd048fff2930d8
kaushik-vishwas <kaushikbiswascse@gmail.com>
2026-09-19 17:06:48 +0530

    dff

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -11,6 +11,7 @@ import CallSession from '../models/CallSession';
 import ReceiverRating from '../models/ReceiverRating';
 import ChatMessage from '../models/ChatMessage';
 import AdminSettings, { type IFixedPerMinuteWindow, type ReceiverEarningModel } from '../models/AdminSettings';
+
 import {
   clearReceiverEarningSettingsCache,
   DEFAULT_FIXED_PER_MINUTE_WINDOWS,
